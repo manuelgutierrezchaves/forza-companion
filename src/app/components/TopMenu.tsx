d93388a0d065618bac4b7a1027e6c1fb7f4cf622
Manu <m.gutierrez@sialitech.com>
2023-10-21 14:52:56 +0200

    profile settings in navbar

--- src/app/components/TopMenu.tsx
+++ src/app/components/TopMenu.tsx
@@ -22,7 +22,27 @@ const TopMenu = () => {
           <li><Link href="/races">Races</Link></li>
         </ul>
       </div>
+
+      <div className="dropdown dropdown-end ml-auto">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="/profile.jpg" />
+            </div>
+          </label>
+          <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
     </div>
+
+
   );
 };
 
