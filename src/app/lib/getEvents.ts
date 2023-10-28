439fc660a05e8e53aee4548f1b3a206d315594e7
Manu <m.gutierrez@sialitech.com>
2023-10-28 14:58:06 +0200

    tabs

--- src/app/lib/getEvents.ts
+++ src/app/lib/getEvents.ts
@@ -2,11 +2,11 @@ import prisma from './prisma'
 
 export async function getEvents(eventType: string) {
 	try {
-    const series = await prisma.events.findMany({
-		where: {
-			type: eventType
-		}
-    });
+		const series = await prisma.events.findMany({
+			where: {
+				type: eventType
+			}
+		});
 		return { series }
 	} catch (error) {
 		return { error }
