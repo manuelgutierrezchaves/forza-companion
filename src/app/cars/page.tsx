e9bc9af85effc9f0b48b2b7dde7313ae34e55573
Manu <m.gutierrez@sialitech.com>
2023-10-29 11:21:52 +0100

    carspage coming soon

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -2,9 +2,14 @@ import Navbar from '@/components/Navbar'
 
 const CarsPage = () => {
   return (
-      <div>
-        <h1>Coches</h1>
+    <div className="pt-20 flex flex-col justify-center items-center p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md text-center max-w-xl w-full">
+        <h1 className="text-4xl font-bold mb-4">Cars</h1>
+        <p className="text-xl text-gray-600">
+          Coming Soon
+        </p>
       </div>
+    </div>
   )
 }
 
