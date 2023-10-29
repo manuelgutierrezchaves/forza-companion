c4652f01949bbe9819eb8ef5a7b887f5e6f93f72
Manu <m.gutierrez@sialitech.com>
2023-10-29 11:17:57 +0100

    ' apos

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,8 +5,8 @@ export default function NotFound() {
     <div className="pt-20 flex flex-col justify-center items-center">
       <div className="bg-white p-8 rounded-lg shadow-md">
         <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
-        <p className="text-xl mb-4">Oops! It seems this page doesn't exist.</p>
-        <a href="/" className="text-blue-500 hover:underline">Return home</a>
+        <p className="text-xl mb-4">Oops! It seems this page doesn&apos;t exist.</p>
+        <Link href="/" className="text-blue-500 hover:underline">Return home</Link>
       </div>
     </div>
   );
